ec5b02372e2a8b655d0afd6d1083ed1e68235b14
wanbo <gewanbo@gmail.com>
2016-06-23 17:40:23 +0800

    Complete Cms2HBase channel.

---
+++ src/main/scala/com/wanbo/channel/Cms2HBase.scala
@@ -0,0 +1,11 @@
+package com.wanbo.channel
+
+import com.wanbo.lib.Content
+
+/**
+  * Created by wanbo on 16/6/20.
+  */
+abstract class Cms2HBase {
+    def addProperties(content: Content)
+    def delProperties(contentId: String)
+}
