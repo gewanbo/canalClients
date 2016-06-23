9b50524050c88d60c38a3858281481d434682c28
wanbo <gewanbo@gmail.com>
2016-06-23 17:39:35 +0800

    Add content classes.

---
+++ src/main/scala/com/wanbo/lib/Content.scala
@@ -0,0 +1,8 @@
+package com.wanbo.lib
+
+/**
+  * Created by wanbo on 16/6/23.
+  */
+abstract class Content {
+
+}
---
+++ src/main/scala/com/wanbo/lib/Story.scala
@@ -0,0 +1,6 @@
+package com.wanbo.lib
+
+/**
+  * Created by wanbo on 16/6/23.
+  */
+case class Story(id: String, cheadline: String) extends Content
